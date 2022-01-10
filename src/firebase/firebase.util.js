18eab5651f776d565b7cc1360cdfefe2b7c692c7
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2022-01-10 22:38:14 +0100

    Update firebase.util.js

--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -15,11 +15,27 @@ const config = {
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if(!userAuth) return;
 
-  const userRef = firestore.doc('users/5r365fv3vj');
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
 
   const snapShot = await userRef.get();
 
-  console.log(snapShot)
+  if(!snapShot.exist){
+    const {displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      })
+    }catch(error){
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
 }
 
 firebase.initializeApp(config);
