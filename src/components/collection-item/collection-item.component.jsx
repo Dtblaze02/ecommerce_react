878381084b16e9d6ff63010699a09ba0df9f267b
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2022-01-12 00:21:47 +0100

    Update collection-item.component.jsx

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import './collection-item.styles.scss';
+import CustomButton from "../custom-button/custom-button.component";
+
 
 const CollectionItem = ({id, name, price, imageUrl}) => (
     <div className="collection-item">
@@ -13,8 +15,8 @@ const CollectionItem = ({id, name, price, imageUrl}) => (
         <div className="collection-footer">
           <span className="name">{name}</span>
           <span className="price">{price}</span>
-        </div>           
-        
+        </div>
+        <CustomButton inverted>Add to cart</CustomButton>
     </div>
 )
 
