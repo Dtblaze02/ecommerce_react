0b54d2c3a3bc5a3e02b1c27bf85e2058fe84b4be
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2022-01-13 11:25:03 +0100

    Update cart-icon.component.jsx

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
+
 import { ReactComponent as ShoppingIcon } from '../../assets/img/shopping-bag.svg';
+
 import './cart-icon.styles.scss';
 
 const CartIcon = ({ toggleCartHidden, itemCount }) => (
