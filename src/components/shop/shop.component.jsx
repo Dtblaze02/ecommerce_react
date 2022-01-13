9bb4465471004e66b0ee9515dc75db67d9a6a937
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2022-01-13 10:50:02 +0100

    Update shop.component.jsx

--- src/components/shop/shop.component.jsx
+++ src/components/shop/shop.component.jsx
@@ -2,12 +2,10 @@ import React from "react";
 
 import CollectionsOverview from "../collections-overview/collections-overview.component";
 
-const ShopPage = ({collections}) => (
-
-    <div className="shop-page">
+const ShopPage = (match) =>(
+    <div className="shop-page"> 
         <CollectionsOverview/>
     </div>
-
- )
-
+    )
+    
 export default ShopPage;
