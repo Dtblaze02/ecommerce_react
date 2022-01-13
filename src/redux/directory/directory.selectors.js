57b96ceac74b5a5130b119ef85c1d803d8f06806
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2022-01-13 11:32:41 +0100

    Update directory.selectors.js

--- src/redux/directory/directory.selectors.js
+++ src/redux/directory/directory.selectors.js
@@ -2,8 +2,7 @@ import { createSelector } from 'reselect';
 
 const selectDirectory = state => state.directory;
 
-const selectDirectorySections = createSelector(
-    [selectDirectory],
-    directory => directory.sections
+export const selectDirectorySections = createSelector(
+  [selectDirectory],
+  directory => directory.sections
 );
- export default selectDirectorySections;
