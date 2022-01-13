899c24e0c15072eec1d94d2d8edff261b345365d
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2022-01-13 11:33:17 +0100

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
-import {persistStore} from 'redux-persist';
 
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-const persistor = persistStore(store);
+export const persistor = persistStore(store);
 
-export {store, persistor};
+export default { store, persistStore };
