5f285396cad671f1b5749bcfd138156ba5b7c28d
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2022-01-14 16:46:28 +0100

    Update header.component.jsx

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,35 +9,46 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 
-import { ReactComponent as Logo } from '../../assets/img/crown.svg';
-
-import './header.styles.scss';
 
 const Header = ({ currentUser, hidden }) => (
-  <div className='header'>
-    <Link className='logo-container' to='/'>
-      <Logo className='logo' />
-    </Link>
-    <div className='options'>
-      <Link className='option' to='/shop'>
-        SHOP
-      </Link>
-      <Link className='option' to='/shop'>
-        CONTACT
-      </Link>
-      {currentUser ? (
-        <div className='option' onClick={() => auth.signOut()}>
-          SIGN OUT
-        </div>
-      ) : (
-        <Link className='option' to='/signin'>
-          SIGN IN
-        </Link>
-      )}
-      <CartIcon />
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light mb-3">
+  <div class="container">
+    <Link className='navbar-brand' to='/'>
+      ShopnGo
+    </Link>    
+    <CartIcon />
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link className='nav-link active' to='/shop'>
+            SHOP
+          </Link>
+        </li>
+        <li class="nav-item">
+          <Link className='nav-link' to='/shop'>
+            CONTACT
+          </Link>
+        </li>
+        <li class="nav-item">
+          {currentUser ? (
+            <div className='nav-link' onClick={() => auth.signOut()}>
+              SIGN OUT
+            </div>
+            ) : (
+          <Link className='nav-link' to='/signin'>
+            SIGN IN
+          </Link>
+           )}
+        </li>
+      </ul>
     </div>
     {hidden ? null : <CartDropdown />}
   </div>
+</nav>
 );
 
 const mapStateToProps = createStructuredSelector({
