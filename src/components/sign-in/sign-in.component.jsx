188929a67536b1d80421ec375131f8ff2643ad8d
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2022-01-14 15:45:15 +0100

    Update sign-in.component.jsx

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
+import { Link } from 'react-router-dom';
 
 import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
-import './sign-in.styles.scss';
+//import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -38,35 +39,45 @@ class SignIn extends React.Component {
 
   render() {
     return (
-      <div className='sign-in'>
-        <h2>I already have an account</h2>
-        <span>Sign in with your email and password</span>
+      <main class="form-signin"> 
+        <div className='sign-in'>
+          <h2>I already have an account</h2>
+          <span>Sign in with your email and password</span>
 
-        <form onSubmit={this.handleSubmit}>
-          <FormInput
-            name='email'
-            type='email'
-            handleChange={this.handleChange}
-            value={this.state.email}
-            label='email'
-            required
-          />
-          <FormInput
-            name='password'
-            type='password'
-            value={this.state.password}
-            handleChange={this.handleChange}
-            label='password'
-            required
-          />
-          <div className='buttons'>
-            <CustomButton type='submit'> Sign in </CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-              Sign in with Google
-            </CustomButton>
-          </div>
-        </form>
-      </div>
+          <form onSubmit={this.handleSubmit}>
+          <div class="form-floating">
+              <FormInput
+                name='email'
+                type='email'
+                handleChange={this.handleChange}
+                value={this.state.email}
+                label='email'
+                required
+              />
+            </div>
+
+            <div class="form-floating">
+              <FormInput
+                name='password'
+                type='password'
+                value={this.state.password}
+                handleChange={this.handleChange}
+                label='password'
+                required
+              />
+            </div>
+            <div className='buttons'>
+              <CustomButton className="w-100 btn btn-lg btn-primary bg-dark mb-3" type='submit'> Sign in </CustomButton>
+              <CustomButton className="w-100 btn btn-lg btn-primary" onClick={signInWithGoogle} isGoogleSignIn>
+                Sign in with Google
+              </CustomButton>
+            </div>            
+            <h5 className='mt-3'> Don't have an Account?  
+            <Link to='/signup'> Create one </Link>
+            </h5>
+          </form>
+        </div>
+      </main>
     );
   }
 }
