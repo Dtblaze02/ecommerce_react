96bf1ae0badb3bba8f43c625d0ebcf7032e88c57
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2022-01-14 15:45:07 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import SignInPage from './pages/sign-in/signin.page';
+import SignUpPage from './pages/sign-up/signup.page';
 import CheckoutPage from './pages/checkout/checkout.component';
 
 import Header from './components/header/header.component';
@@ -50,6 +51,8 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route path='/signup' component={SignUpPage} />
+          <Route path='/signin' component={SignInPage} />
           <Route exact path='/checkout' component={CheckoutPage} />
           <Route
             exact
@@ -58,7 +61,7 @@ class App extends React.Component {
               this.props.currentUser ? (
                 <Redirect to='/' />
               ) : (
-                <SignInAndSignUpPage />
+                <SignInPage />
               )
             }
           />
