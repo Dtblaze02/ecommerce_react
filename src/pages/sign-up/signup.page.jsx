97ac25a5c4019c1e8e3f04154bb70368eeb78c73
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2022-01-14 15:57:21 +0100

    Create signup.page.jsx

---
+++ src/pages/sign-up/signup.page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignUp from '../../components/sign-up/sign-up.component';
+
+//import './sign-in-and-sign-up.styles.scss';
+
+const SignUpPage = () => (
+  <div className='sign-in-and-sign-up'>
+    <SignUp />
+  </div>
+);
+
+export default SignUpPage;
