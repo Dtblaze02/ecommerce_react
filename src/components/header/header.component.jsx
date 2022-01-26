254e96131a2cd4b8c9a30c13b834198d7dcebdc8
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2022-01-26 21:36:06 +0100

    Update header.component.jsx

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,72 +9,75 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 import 'bootstrap/dist/css/bootstrap.css'
+import Hamburger from './hamburger';
 
 
 const Header = ({ currentUser, hidden }) => (
-
-<header className="header trans_300">        
-    <div className="main_nav_container">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12 text-right">
-            <div className="logo_container">
-            <Link to='/'>
-              Shop<span>N</span>Go
-            </Link>
-            </div>
-            <nav className="navbar">
-              <ul className="navbar_menu">
-                <li>  
-                  <Link to='/shop'>
-                    SHOP
-                  </Link>
-                </li>
-                <li>
-                  <Link to='#'>
-                    PROMOTIONS
-                  </Link>
-                </li>
-                <li>
-                  <Link to='#'>
-                    BLOG
-                  </Link>
-                </li>
-                <li>
-                  <Link to='/shop'>
-                    CONTACT
-                  </Link>
-                </li>                      
-              </ul>
-              <ul className="navbar_user">
-                <li>
-                  <Link to='#'>
-                    <i className="fa fa-search" aria-hidden="true"></i>
-                  </Link>
-                </li>
-                <li>
-                  {currentUser ? (
-                    <span className='nav-link' onClick={() => auth.signOut()}>
-                      sign out
-                    </span>
-                    ) : (
-                  <Link to='/signin'>
-                    <i className="fa fa-user" aria-hidden="true"></i>                          
-                  </Link>
-                    )}
-                </li>
-                  <CartIcon/>
-                  {hidden ? null : <CartDropdown />}
-              </ul>
-              <div className="hamburger_container">
-                <i className="fa fa-bars" aria-hidden="true"></i>
+<>
+  <header className="header trans_300">        
+      <div className="main_nav_container">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12 text-right">
+              <div className="logo_container">
+              <Link to='/'>
+                Shop<span>N</span>Go
+              </Link>
               </div>
-            </nav>
+              <nav className="navbar">
+                <ul className="navbar_menu">
+                  <li>  
+                    <Link to='/shop'>
+                      SHOP
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to='#'>
+                      PROMOTIONS
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to='#'>
+                      BLOG
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to='/shop'>
+                      CONTACT
+                    </Link>
+                  </li>                      
+                </ul>
+                <ul className="navbar_user">
+                  <li>
+                    <Link to='#'>
+                      <i className="fa fa-search" aria-hidden="true"></i>
+                    </Link>
+                  </li>
+                  <li>
+                    {currentUser ? (
+                      <span className='nav-link' onClick={() => auth.signOut()}>
+                        sign out
+                      </span>
+                      ) : (
+                    <Link to='/signin'>
+                      <i className="fa fa-user" aria-hidden="true"></i>                          
+                    </Link>
+                      )}
+                  </li>
+                    <CartIcon/>
+                    {hidden ? null : <CartDropdown />}
+                </ul>
+                <div className="hamburger_container">
+                  <i className="fa fa-bars" aria-hidden="true"></i>
+                </div>
+              </nav>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-</header>
+  </header>
+  <Hamburger/>
+</>
 );
 
 const mapStateToProps = createStructuredSelector({
