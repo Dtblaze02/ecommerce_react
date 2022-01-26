9eff0856f6b627ff14b5c2d894b90d2b7d27daf8
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2022-01-26 11:07:16 +0100

    Update header.component.jsx

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,47 +8,74 @@ import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/js/src/dom/manipulator'
 
 
 const Header = ({ currentUser, hidden }) => (
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light mb-3">
-  <div class="container">
-    <Link className='navbar-brand' to='/'>
-      ShopnGo
-    </Link>    
-    <CartIcon />
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <Link className='nav-link active' to='/shop'>
-            SHOP
-          </Link>
-        </li>
-        <li class="nav-item">
-          <Link className='nav-link' to='/shop'>
-            CONTACT
-          </Link>
-        </li>
-        <li class="nav-item">
-          {currentUser ? (
-            <div className='nav-link' onClick={() => auth.signOut()}>
-              SIGN OUT
+<header class="header trans_300">        
+    <div class="main_nav_container">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-right">
+            <div class="logo_container">
+            <Link to='/'>
+              Shop<span>N</span>Go
+            </Link>
             </div>
-            ) : (
-          <Link className='nav-link' to='/signin'>
-            SIGN IN
-          </Link>
-           )}
-        </li>
-      </ul>
+            <nav class="navbar">
+              <ul class="navbar_menu">
+                <li>  
+                  <Link to='/shop'>
+                    SHOP
+                  </Link>
+                </li>
+                <li>
+                  <Link to='#'>
+                    PROMOTIONS
+                  </Link>
+                </li>
+                <li>
+                  <Link to='#'>
+                    BLOG
+                  </Link>
+                </li>
+                <li>
+                  <Link to='/shop'>
+                    CONTACT
+                  </Link>
+                </li>                      
+              </ul>
+              <ul class="navbar_user">
+                <li>
+                  <Link to='#'>
+                    <i class="fa fa-search" aria-hidden="true"></i>
+                  </Link>
+                </li>
+                <li>
+                  {currentUser ? (
+                    <div className='nav-link' onClick={() => auth.signOut()}>
+                      SIGN OUT
+                    </div>
+                    ) : (
+                  <Link className='nav-link' to='/signin'>
+                    <i class="fa fa-user" aria-hidden="true"></i>                          
+                  </Link>
+                    )}
+                </li>
+                  <CartIcon/>
+                  {hidden ? null : <CartDropdown />}
+              </ul>
+              <div class="hamburger_container">
+                <i class="fa fa-bars" aria-hidden="true"></i>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </div>
     </div>
-    {hidden ? null : <CartDropdown />}
-  </div>
-</nav>
+</header>
 );
 
 const mapStateToProps = createStructuredSelector({
